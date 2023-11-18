0d3a6ef3ba58cfcd4f56971b8f43ee9103ac2624
bikram <debbikramjitofficial@gmail.com>
2023-11-18 23:32:30 +0530

    Commit-4

--- Welcome.java
+++ Welcome.java
@@ -25,7 +25,7 @@ Hello, Java.
  * 
  */
 
-public class Welcome_2 {
+public class Welcome {
 
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
